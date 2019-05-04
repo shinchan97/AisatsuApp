68bcdb876ce377bf12f22d4a14d7dd932335286e
Arata Ueno <arata.ueno@gmail.com>
2019-05-03 18:51:27 -0700

    課題(Lesson 4 part 1) 未完

---
+++ app/src/main/java/com/example/aisatsuapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.aisatsuapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.app.TimePickerDialog
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button1) {
+            showTimePickerDialog()
+
+        }
+    }
+
+    private fun showTimePickerDialog(){
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                Log.d("UI_PARTS", "$hour:$minute")
+            },
+            13, 0, true)
+        timePickerDialog.show()
+        
+    }
+}
